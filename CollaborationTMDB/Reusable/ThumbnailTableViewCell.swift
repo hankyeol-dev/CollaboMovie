4b8840d37d8d7ebd7e0914082bb12ced7fd865f5
hankyeol-dev <ddovlek@kakao.com>
2024-10-08 22:26:09 +0900

    💻 Feat: BaseTableviewCell, ThumbnailTableViewCell 구현

---
+++ CollaborationTMDB/Reusable/ThumbnailTableViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  ThumbnailTableViewCell.swift
+//  CollaborationTMDB
+//
+//  Created by 강한결 on 10/8/24.
+//
+
+import UIKit
+
+import SnapKit
+import Kingfisher
+
+struct MovieModel: Decodable {
+   var path: String
+   var title: String
+}
+
+final class ThumbnailTableViewCell: BaseTableViewCell {
+   static let id: String = "ThumbnailTableViewCell"
+   
+   private let thumbnail: UIImageView = {
+      let view = UIImageView()
+      view.contentMode = .scaleToFill
+      return view
+   }()
+   private let titleLabel: UILabel = {
+      let view = UILabel()
+      view.font = .systemFont(ofSize: 16.0, weight: .regular)
+      return view
+   }()
+   private let playButton: RoundedButton = .init(
+      foregroundColor: .black,
+      backgroundColor: .clear,
+      title: "",
+      fontSize: 20.0,
+      image: .playCircle
+   )
+   
+   override func configureHierachy() {
+      super.configureHierachy()
+      contentView.addSubviews(thumbnail, titleLabel, playButton)
+   }
+   
+   override func configureLayout() {
+      super.configureLayout()
+      thumbnail.snp.makeConstraints { make in
+         make.verticalEdges.leading.equalTo(contentView.safeAreaLayoutGuide).inset(15.0)
+         make.width.equalTo(100.0)
+      }
+      titleLabel.snp.makeConstraints { make in
+         make.centerY.equalTo(thumbnail.snp.centerY)
+         make.leading.equalTo(thumbnail.snp.trailing).offset(15.0)
+         make.trailing.equalTo(playButton.snp.leading).offset(-15.0)
+      }
+      playButton.snp.makeConstraints { make in
+         make.centerY.equalTo(thumbnail.snp.centerY)
+         make.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(15.0)
+         make.width.equalTo(24.0)
+      }
+   }
+   
+   func configureView(_ data: MovieModel) {
+      thumbnail.kf.setImage(with: URL(string: data.path))
+      titleLabel.text = data.title
+   }
+   
+   func configrueViewWithLocalData(_ data: MovieModel) {
+      thumbnail.image = UIImage(contentsOfFile: data.path)
+      titleLabel.text = data.title
+   }
+}
---
+++ CollaborationTMDB/Utility/+UIView.swift
@@ -0,0 +1,14 @@
+//
+//  +UIView.swift
+//  CollaborationTMDB
+//
+//  Created by 강한결 on 10/8/24.
+//
+
+import UIKit
+
+extension UIView {
+   func addSubviews(_ views: UIView...) {
+      views.forEach { self.addSubview($0) }
+   }
+}
