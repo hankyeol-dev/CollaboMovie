3925ac55b7f9dba3ff17ca80d0615190cb0fb922
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-10-10 20:49:05 +0900

    💾 Add: 홈화면에서 사용되는 티비 모델 매핑 메서드 추가 구성

--- CollaborationTMDB/Model/Trending/Tv/TrendingTVResponseDTO.swift
+++ CollaborationTMDB/Model/Trending/Tv/TrendingTVResponseDTO.swift
@@ -13,4 +13,7 @@ struct TrendingTVResponseDTO: Decodable {
 
 extension TrendingTVResponseDTO {
     // TODO: Mapping Property
+    func toHomeTVs() -> [HomeTV] {
+        return self.results.map { $0.toHomeTV() }
+    }
 }
