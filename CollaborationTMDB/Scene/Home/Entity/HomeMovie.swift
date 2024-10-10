0b5a4c043bf95007c390500d09588dd2be22154d
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-10-10 20:50:44 +0900

    💾 Add: 홈화면에서 사용되는 tv, 영화, 장르 모델 구성

---
+++ CollaborationTMDB/Scene/Home/Entity/HomeMovie.swift
@@ -0,0 +1,16 @@
+//
+//  HomeMovie.swift
+//  CollaborationTMDB
+//
+//  Created by Minjae Kim on 10/10/24.
+//
+
+import Foundation
+
+struct HomeMovie {
+    let id: Int
+    let title: String
+    let posterPath: String
+    let backdropPath: String
+    var genreIds: [Int]
+}
---
+++ CollaborationTMDB/Scene/Home/Entity/HomeTV.swift
@@ -0,0 +1,16 @@
+//
+//  HomeTV.swift
+//  CollaborationTMDB
+//
+//  Created by Minjae Kim on 10/10/24.
+//
+
+import Foundation
+
+struct HomeTV {
+    let id: Int
+    let title: String
+    let posterPath: String
+    let backdropPath: String
+    var genreIds: [Int]
+}
