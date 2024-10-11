e12d21fb178649d5dc03b21fe49815d7d514e53a
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2024-10-11 09:31:03 +0900

    🩹 Refactor: 메인 브랜치에서도 변동이 있어 같이 수정

--- CollaborationTMDB/Database/MovieTable.swift
+++ CollaborationTMDB/Database/MovieTable.swift
@@ -11,12 +11,22 @@ import RealmSwift
 final class MovieTable: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var id: Int
     @Persisted var title: String
-    @Persisted var ImagePath: String
+    @Persisted var imageName: String
+    @Persisted var overview: String
+    @Persisted var voteAverage: Double
     
-    convenience init(id: Int, title: String, ImagePath: String) {
+    convenience init(
+        id: Int,
+        title: String,
+        imageName: String,
+        overview: String,
+        voteAverage: Double
+    ) {
         self.init()
         self.id = id
         self.title = title
-        self.ImagePath = ImagePath
+        self.imageName = imageName
+        self.overview = overview
+        self.voteAverage = voteAverage
     }
 }
