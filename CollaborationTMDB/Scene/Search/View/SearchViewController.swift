661825c7ce1f3612fdc5e1439a52671ec1938346
ray <audi3m@naver.com>
2024-10-14 13:26:05 +0900

    💾 Add: 검색 페이지네이션 추가, 검색어 없을 시 트렌드 영화 테이블뷰 추가, 검색 결과 없음 레이블 추가

--- CollaborationTMDB/Scene/Search/View/SearchViewController.swift
+++ CollaborationTMDB/Scene/Search/View/SearchViewController.swift
@@ -17,12 +17,28 @@ final class SearchViewController: UIViewController {
         bar.backgroundImage = UIImage()
         return bar
     }()
+    private lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.register(ThumbnailTableViewCell.self, forCellReuseIdentifier: ThumbnailTableViewCell.id)
+        view.separatorStyle = .none
+        view.rowHeight = 180
+        view.isHidden = false
+        return view
+    }()
     private lazy var collectionView: UICollectionView = {
         let view = UICollectionView(frame: .zero, collectionViewLayout: SearchViewController.layout())
         view.delegate = self
+        view.isHidden = true
         view.register(VerticalCollectionViewCell.self, forCellWithReuseIdentifier: VerticalCollectionViewCell.id)
         view.register(SearchHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SearchHeader.id)
         return view
+    }()
+    private let noResultsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "결과가 없습니다."
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
     }()
     
     let disposeBag = DisposeBag()
@@ -33,6 +49,8 @@ final class SearchViewController: UIViewController {
         configureHierarchy()
         configureLayout()
         configureUI()
+        viewModel.action(.viewDidLoad)
+        configureTableViewHeader()
         rxBind()
     }
     
@@ -41,47 +59,104 @@ final class SearchViewController: UIViewController {
 // RxBind
 extension SearchViewController {
     private func rxBind() {
-        let input = SearchViewModel.Input(query: searchBar.rx.text.orEmpty)
-        let output = viewModel.transform(input: input)
+        searchBar.rx.text.orEmpty
+            .bind(to: viewModel.input.searchQuery)
+            .disposed(by: disposeBag)
         
-        output.movieList
-            .drive(collectionView.rx.items(cellIdentifier: VerticalCollectionViewCell.id, cellType: VerticalCollectionViewCell.self)) { (row, movie, cell) in
+        viewModel.output.trendMovieData
+            .bind(to: tableView.rx.items(cellIdentifier: ThumbnailTableViewCell.id, cellType: ThumbnailTableViewCell.self)) { index, movie, cell in
+                cell.configureView(movie)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.output.trendCellTapData
+            .bind(with: self) { owner, detailViewInput in
+                let detailView: DetailViewController = .init(
+                    detailViewModel: .init(detailViewInput: detailViewInput)
+                )
+                owner.present(detailView, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .modelSelected(HomeMedia.self)
+            .bind(with: self) { owner, movie in
+                owner.viewModel.action(.trendCellTap(movie))
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.output.searchedMovieData
+            .bind(to: collectionView.rx.items(cellIdentifier: VerticalCollectionViewCell.id, cellType: VerticalCollectionViewCell.self)) { index, movie, cell in
                 cell.configureData(movie.posterPath)
             }
             .disposed(by: disposeBag)
         
-        collectionView.rx.modelSelected(TMDBMovieResponseDTO.self)
-            .subscribe(with: self, onNext: { owner, movie in
-                let detailViewInput: DetailViewInput = .init(
-                    id: movie.id,
-                    title: movie.title,
-                    backdropPath: movie.backdropPath,
-                    voteAverage: movie.voteAverage,
-                    overview: movie.overview,
-                    mediaType: .movie)
-                
-                var targetInput = detailViewInput
-                targetInput.mapIsSavedMedia()
-                
-                let detailView = DetailViewController(
-                    detailViewModel: .init(detailViewInput: targetInput)
+        viewModel.output.movieCellTapData
+            .bind(with: self) { owner, detailViewInput in
+                let detailView: DetailViewController = .init(
+                    detailViewModel: .init(detailViewInput: detailViewInput)
                 )
                 owner.present(detailView, animated: true)
-            })
+            }
+            .disposed(by: disposeBag)
+        
+        collectionView.rx
+            .modelSelected(SearchMedia.self)
+            .bind(with: self) { owner, movie in
+                owner.viewModel.action(.movieCellTap(movie))
+            }
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            searchBar.rx.text.orEmpty,
+            viewModel.searched,
+            viewModel.output.searchedMovieData
+        )
+        .observe(on: MainScheduler.instance)
+        .subscribe(onNext: { [weak self] query, searched, movies in
+            guard let self = self else { return }
+            if query.isEmpty {
+                self.tableView.isHidden = false
+                self.collectionView.isHidden = true
+                self.noResultsLabel.isHidden = true
+            } else {
+                self.tableView.isHidden = true
+                if searched {
+                    self.collectionView.isHidden = movies.isEmpty
+                    self.noResultsLabel.isHidden = !movies.isEmpty
+                } else {
+                    self.noResultsLabel.isHidden = true
+                }
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        collectionView.rx
+            .didScroll
+            .withLatestFrom(collectionView.rx.contentOffset)
+            .filter { [weak self] offset in
+                guard let self else { return false }
+                let position = offset.y + self.collectionView.frame.size.height
+                return position >= self.collectionView.contentSize.height
+            }
+            .map { _ in }
+            .bind(to: viewModel.input.loadNextPage)
             .disposed(by: disposeBag)
     }
 }
 
 // CollectionView
-extension SearchViewController: UICollectionViewDelegate {
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         guard kind == UICollectionView.elementKindSectionHeader else {
             return UICollectionReusableView()
         }
-        
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: SearchHeader.id, for: indexPath) as! SearchHeader
-        header.setTitle(with: "영화 & 시리즈")
         return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 44)
     }
 }
 
@@ -89,7 +164,9 @@ extension SearchViewController {
     
     private func configureHierarchy() {
         view.addSubview(searchBar)
+        view.addSubview(tableView)
         view.addSubview(collectionView)
+        view.addSubview(noResultsLabel)
     }
     
     private func configureLayout() {
@@ -98,9 +175,17 @@ extension SearchViewController {
             make.horizontalEdges.equalToSuperview()
             make.height.equalTo(44)
         }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(searchBar.snp.bottom)
             make.horizontalEdges.bottom.equalToSuperview()
+        }
+        noResultsLabel.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
         }
     }
     
@@ -124,6 +209,26 @@ extension SearchViewController {
         layout.headerReferenceSize = CGSize(width: screenWidth, height: 44)
         
         return layout
+    }
+    
+    private func configureTableViewHeader() {
+        let headerContainer = UIView()
+        headerContainer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 44)
+        
+        let headerLabel = UILabel()
+        headerLabel.text = "추천 시리즈 & 영화"
+        headerLabel.font = .boldSystemFont(ofSize: 20)
+        headerLabel.textColor = .label
+        headerLabel.textAlignment = .left
+        
+        headerContainer.addSubview(headerLabel)
+        
+        headerLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(10)
+            make.centerY.equalToSuperview()
+        }
+        
+        tableView.tableHeaderView = headerContainer
     }
 }
 
