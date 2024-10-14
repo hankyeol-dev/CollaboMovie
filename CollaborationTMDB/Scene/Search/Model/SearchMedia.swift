d52cc06bbe47a71c27f02def96ac9e04ecb86b20
ray <audi3m@naver.com>
2024-10-14 13:24:47 +0900

    💾 Add: 검색 화면에서 사용할 모델 추가

--- CollaborationTMDB/Scene/Search/Model/SearchMedia.swift
+++ CollaborationTMDB/Scene/Search/Model/SearchMedia.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct SearchMedia {
+    let id: Int
+    let title: String
+    let posterPath: String
+    let backdropPath: String
+    var genreIds: [Int]
+    var overview: String
+    var voteAverage: Double
+}
