f07c78ef5805c283bc13a03a02f1091c09270d3c
ray <audi3m@naver.com>
2024-10-14 13:27:29 +0900

    💾 Add: 검색 뷰모델 페이지네이션 로직, 초기 트렌드 영화 불러오기 로직, 디테일 모달 띄우기 로직 추가

--- CollaborationTMDB/Scene/Search/ViewModel/SearchViewModel.swift
+++ CollaborationTMDB/Scene/Search/ViewModel/SearchViewModel.swift
@@ -10,53 +10,165 @@ import RxSwift
 import RxCocoa
 
 final class SearchViewModel {
+    private let tmdbRepository: TMDBRepository
+    private let disposeBag: DisposeBag
+    private let searchPage = BehaviorRelay<Int>(value: 1)
     
-    let disposeBag = DisposeBag()
+    var input = Input()
+    var output = Output()
     
-    func transform(input: Input) -> SingleOutput {
-        let result = input.query
-            .distinctUntilChanged()
-            .flatMap { [weak self] query in
-                guard let self else { return Single<SearchMovieResponseDTO>.never() }
-                return self.requestSearchSingle(query: query, page: 1)
-                    .catch { _ in
-                        return Single<SearchMovieResponseDTO>.never()
-                    }
-            }
-            .asDriver(onErrorJustReturn: SearchMovieResponseDTO(page: 1, results: [], totalPage: 1))
-            .debug("Query changed")
-        
-        return SingleOutput(movieList: result)
-    }
+    var isLastPage = false
+    var searched = BehaviorRelay<Bool>(value: false)
     
-    private func requestSearchSingle(query: String, page: Int) -> Single<SearchMovieResponseDTO> {
-        return Single.create { observer in
-            TMDBRepository.shared.requestSearch(query: query, page: page) { result in
-                switch result {
-                case .success(let success):
-                    observer(.success(success))
-                case .failure(let error):
-                    observer(.failure(error))
-                }
-            }
-            return Disposables.create()
-        }
-        .debug("requestSearchSingle")
+    init() {
+        self.tmdbRepository = TMDBRepository.shared
+        self.disposeBag = DisposeBag()
+        transform()
     }
 }
- 
+    
 extension SearchViewModel {
     struct Input {
-        let query: ControlProperty<String>
+        var viewDidLoad = PublishRelay<Void>()
+        var searchQuery = PublishRelay<String>()
+        var loadNextPage = PublishRelay<Void>()
+        var movieCellTap = PublishRelay<SearchMedia>()
+        var trendCellTap = PublishRelay<HomeMedia>()
     }
     
-    struct SingleOutput {
-        let movieList: Driver<SearchMovieResponseDTO>
+    struct Output {
+        var searchedMovieData = BehaviorRelay<[SearchMedia]>(value: [])
+        var trendMovieData = PublishRelay<[HomeMedia]>()
+        var movieCellTapData = PublishRelay<DetailViewInput>()
+        var trendCellTapData = PublishRelay<DetailViewInput>()
+    }
+    
+    func transform() {
+        input.viewDidLoad
+            .bind(with: self) { owner, _ in
+                owner.fetchTrendData()
+            }
+            .disposed(by: disposeBag)
+        
+        input.searchQuery
+            .distinctUntilChanged()
+            .bind(with: self) { owner, _ in
+                owner.searched.accept(false)
+            }
+            .disposed(by: disposeBag)
+        
+        input.searchQuery
+            .distinctUntilChanged()
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
+            .bind(with: self) { owner, query in
+                owner.isLastPage = false
+                owner.searched.accept(false)
+                owner.fetchSearchData(query: query, page: 1)
+            }
+            .disposed(by: disposeBag)
+        
+        input.loadNextPage
+            .withLatestFrom(input.searchQuery)
+            .filter { [weak self] _ in
+                guard let self else { return false }
+                return self.isLastPage == false
+            }
+            .bind(with: self) { owner, query in
+                let nextPage = owner.searchPage.value + 1
+                owner.searchPage.accept(nextPage)
+                owner.fetchSearchData(query: query, page: nextPage)
+            }
+            .disposed(by: disposeBag)
+        
+        input.movieCellTap
+            .bind(with: self) { owner, movie in
+                var detailViewInput: DetailViewInput = .init(
+                    id: movie.id,
+                    title: movie.title,
+                    backdropPath: movie.backdropPath,
+                    voteAverage: movie.voteAverage,
+                    overview: movie.overview,
+                    mediaType: .movie)
+                detailViewInput.mapIsSavedMedia()
+                owner.output.movieCellTapData.accept(detailViewInput)
+            }
+            .disposed(by: disposeBag)
+        
+        input.trendCellTap
+            .bind(with: self) { owner, movie in
+                var detailViewInput: DetailViewInput = .init(
+                    id: movie.id,
+                    title: movie.title,
+                    backdropPath: movie.backdropPath,
+                    voteAverage: movie.voteAverage,
+                    overview: movie.overview,
+                    mediaType: .movie)
+                detailViewInput.mapIsSavedMedia()
+                owner.output.trendCellTapData.accept(detailViewInput)
+            }
+            .disposed(by: disposeBag)
     }
 }
 
-extension SearchMovieResponseDTO: Sequence {
-    func makeIterator() -> Array<TMDBMovieResponseDTO>.Iterator {
-        return results.makeIterator()
+extension SearchViewModel {
+    enum Action {
+        case viewDidLoad
+        case movieCellTap(_ movie: SearchMedia)
+        case trendCellTap(_ movie: HomeMedia)
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .viewDidLoad:
+            input.viewDidLoad.accept(())
+        case .movieCellTap(let movie):
+            input.movieCellTap.accept(movie)
+        case .trendCellTap(let movie):
+            input.trendCellTap.accept(movie)
+        }
+    }
+}
+
+extension SearchViewModel {
+    private func fetchSearchData(query: String, page: Int) {
+        tmdbRepository.requestSearchMovie(query: query, page: page)
+            .asDriver(onErrorJustReturn: .failure(.badRequest))
+            .drive(with: self) { owner, result in
+                switch result {
+                case .success(let data):
+                    let currentData = page == 1 ? [] : owner.output.searchedMovieData.value
+                    owner.output.searchedMovieData.accept(currentData + data.results)
+                    owner.searched.accept(true)
+                    if data.page >= data.totalPage {
+                        owner.isLastPage = true
+                    }
+                case .failure(let error):
+                    print("fetchMovie", error)
+                    owner.searched.accept(true)
+                }
+            } onCompleted: { _ in
+                print("searchMovie onCompleted")
+            } onDisposed: { _ in
+                print("searchMovie onDisposed")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func fetchTrendData() {
+        tmdbRepository.requestTrendingMovie()
+            .asDriver(onErrorJustReturn: .failure(.badRequest))
+            .drive(with: self) { owner, result in
+                switch result {
+                case .success(let data):
+                    owner.output.trendMovieData.accept(data)
+                case .failure(let error):
+                    print("fetchMovie", error)
+                }
+            } onCompleted: { _ in
+                print("fetchMovie onCompleted")
+            } onDisposed: { _ in
+                print("fetchMovie onDisposed")
+            }
+            .disposed(by: disposeBag)
     }
 }
